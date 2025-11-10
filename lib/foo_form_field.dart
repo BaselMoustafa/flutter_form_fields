744e71b24416dc9e0afab0e356cb61e5eabe6f12
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-11-10 20:05:13 +0200

    Remove unused exports from foo_form_field.dart to streamline codebase

--- lib/foo_form_field.dart
+++ lib/foo_form_field.dart
@@ -1,11 +1,9 @@
 export 'src/core/formatters/integer_text_input_formatter.dart';
 export 'src/core/formatters/integer_value_input_formatter.dart';
 
-export 'src/core/models/range.dart';
 export 'src/core/models/range_validator.dart';
 
 export 'src/form_fields/implementations/int_range_form_field.dart';
-export 'src/form_fields/implementations/int_text_form_field.dart';
 export 'src/form_fields/implementations/boolean_form_field.dart';
 export 'src/form_fields/implementations/int_string_form_field.dart';
 export 'src/form_fields/base/foo_string_form_field.dart';
@@ -14,6 +12,5 @@ export 'src/form_fields/base/foo_string_form_field.dart';
 export 'src/core/controllers/base/value_field_controller.dart';
 export 'src/core/controllers/base/foo_field_controller.dart';
 export 'src/core/controllers/base/range_field_controller.dart';
-export 'src/core/controllers/base/foo_text_editing_controller.dart';
 
 
