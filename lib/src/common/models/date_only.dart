9504230772d6d7b57d64dbb7a97b42b71d2afc71
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-12-31 14:55:07 +0200

    Add DateOnly model for date representation and comparison
    Introduce a new DateOnly class that encapsulates year, month, and day attributes, implementing Comparable for date comparisons. This addition enhances date handling capabilities in the application.

---
+++ lib/src/common/models/date_only.dart
@@ -0,0 +1,50 @@
+class DateOnly implements Comparable<DateOnly> {
+  
+  final int year;
+  final int month;
+  final int day;
+
+  DateOnly({
+    required this.year, 
+    required this.month, 
+    required this.day,
+  }): assert(year >0, "Year must be greater than 0"),
+    assert(month >0 && month <= 12, "Month must be between 1 and 12"),
+    assert(day >0 && day <= 31, "Day must be between 1 and 31");
+
+  @override
+  int compareTo(DateOnly other) {
+    if (year != other.year) {
+      return year.compareTo(other.year);
+    }
+    if (month != other.month) {
+      return month.compareTo(other.month);
+    }
+    return day.compareTo(other.day);
+  }
+
+  @override
+  bool operator ==(covariant DateOnly other) {
+    return compareTo(other)==0;
+  }
+
+  bool operator >(DateOnly other) {
+    return compareTo(other) > 0;
+  }
+
+  bool operator >=(DateOnly other) {
+    return compareTo(other) >= 0;
+  }
+
+  bool operator <=(DateOnly other) {
+    return compareTo(other) <= 0;
+  }
+
+  bool operator <(DateOnly other) {
+    return compareTo(other) < 0;
+  }
+
+  @override
+  int get hashCode => year.hashCode ^ month.hashCode ^ day.hashCode;
+
+}
