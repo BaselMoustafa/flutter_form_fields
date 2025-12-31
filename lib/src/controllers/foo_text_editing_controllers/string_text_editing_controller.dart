89310a52e1e6941864eb5556f44c7af811f5b824
Basel Moustafa <basel.moustafa2000@gmail.com>
2025-12-31 12:48:12 +0200

    Add StringTextEditingController for string input handling
    Introduce StringTextEditingController extending FooTextEditingController to manage string inputs with initial value, enabled state, and error text handling. This addition enhances the form field capabilities for string data types.

---
+++ lib/src/controllers/foo_text_editing_controllers/string_text_editing_controller.dart
@@ -0,0 +1,12 @@
+import '../../../foo_form_field.dart';
+
+class StringTextEditingController extends FooTextEditingController<String> {
+  
+  StringTextEditingController({
+    super.initialValue,
+    super.enabled,
+    super.forcedErrorText,
+  }):super(
+    mapper: Mapper.sameValueMapper<String>()
+  );
+}
